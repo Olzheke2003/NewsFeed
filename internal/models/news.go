26893144c5e83429d5e44289b74b5b1eaab23b85
Olzhas <106257014+Olzheke2003@users.noreply.github.com>
2025-01-08 12:19:11 +0500

    gofumt and add models like

--- internal/models/news.go
+++ internal/models/news.go
@@ -2,28 +2,29 @@ package models
 
 import "time"
 
-type Category struct {
-	ID            int    `json:id`
-	Category_name string `json:category_name`
+type Like struct {
+	ID         int       `json:"id"`
+	UserID     int       `json:"user_id"`     // ID пользователя, который поставил лайк
+	ObjectID   int       `json:"object_id"`   // ID объекта (новости или комментария)
+	ObjectType string    `json:"object_type"` // Тип объекта: "news" или "comment"
+	CreatedAt  time.Time `json:"created_at"`  // Время, когда был поставлен лайк
 }
 
 type News struct {
-	ID         int       `json:id`
-	Title      string    `json:title`
-	CategoryID Category  `json:category_id`
-	CreatedAt  time.Time `json:created_at`
+	ID         int       `json:"id"`
+	Title      string    `json:"title"`
+	CategoryID int       `json:"category_id"`
+	CreatedAt  time.Time `json:"created_at"`
 	Content    string    `json:"content"`
 	Image      string    `json:"image"`
-	Likes      int       `json:likes`
 }
 
 type Comments struct {
 	ID       int        `json:"id"`
-	NewsID   News       `json:"news_id"`
+	NewsID   int        `json:"news_id"`
 	ParentID *int       `json:"parent_id"`
-	UserID   Users      `json:"user_id"`
+	UserID   int        `json:"user_id"`
 	Content  string     `json:"content"`
 	DatePost string     `json:"date_post"`
 	Replies  []Comments `json:"replies"`
-	Likes    int        `json:likes`
 }
